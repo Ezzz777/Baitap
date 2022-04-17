839c7a68704cdd4c507474292ea86fe9ef05060d
Ezzz777 <102588323+Ezzz777@users.noreply.github.com>
2022-04-17 22:15:18 +0700

    Code intensive

---
+++ codeintensive.js
@@ -0,0 +1,10 @@
+
+// bài 2
+let m = prompt("nhập số m (1<m<10)")
+function numberOneTriangle(m){
+    let line = ""
+    for (let i=1; i<=m;i++){
+        line = line + "*"
+        console.log(line);
+    }
+} numberOneTriangle(m)
